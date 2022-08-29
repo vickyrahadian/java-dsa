628b2d1c4a4bcd783c919b037509683ab5551572
Vicky <vicky.rahadian@gmail.com>
2022-08-29 19:07:29 -0400

    shell sorting

---
+++ sorting/ShellSorting.java
@@ -0,0 +1,19 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class ShellSorting {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        int gap = numbers.length / 2;
+
+
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}
